2b4878c07f38cdfd47503dc8071af9e5ff0046c4
janko12 <henrich.sury97@gmail.com>
2023-05-31 17:28:50 +0200

    Add and Style Card markup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,10 @@
-export default function Card() {
-    return <div>Card</div>;
+import CardStyles from "./Card.module.css";
+
+export default function Card({ image, name }) {
+    return (
+        <div className={CardStyles.card}>
+            <img src={image} alt={name} />
+            <span className={CardStyles.name}>{name}</span>
+        </div>
+    );
 }
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,11 @@
+import Card from "./Card";
 import CardContainerStyles from "./CardContainer.module.css";
+import anneImage from "../images/anne.jpg";
 
 export default function CardContainer() {
     return (
-        <div className={CardContainerStyles["card-container"]}></div>
+        <div className={CardContainerStyles["card-container"]}>
+            <Card image={anneImage} name={"anne boonchuy"} />
+        </div>
     );
 }
