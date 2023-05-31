63906a7ff7391fe11ec2c3092b07a4379035f9f2
janko12 <henrich.sury97@gmail.com>
2023-05-31 15:58:10 +0200

    Create basic project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./style.css";
+import Header from "./components/Header.jsx";
+import CardContainer from "./components/CardContainer.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-    return <div className="App">hi</div>;
+    return (
+        <div className="App">
+            <Header />
+            <CardContainer />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
