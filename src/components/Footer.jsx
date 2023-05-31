9972f605607cddefbcff494ed0b04ef23f425997
janko12 <henrich.sury97@gmail.com>
2023-06-01 01:46:39 +0200

    Add and style footer markup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,36 @@
+import FooterStyles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub,
+    faFacebook,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className={FooterStyles.footer}>
+            <span>Contact:</span>
+            <a
+                href="https://github.com/heno-s"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a
+                href="https://facebook.com/henoplastoveretazky.sury"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+                href="https://www.instagram.com/henosury/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon icon={faInstagram} />
+            </a>
+        </footer>
+    );
 }
