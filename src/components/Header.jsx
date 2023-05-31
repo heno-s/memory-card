82b50b3bb60a309444954ab82c6d62e18295a3e9
janko12 <henrich.sury97@gmail.com>
2023-05-31 16:45:57 +0200

    Add Header markup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,16 @@
+import ScoreBoard from "./ScoreBoard.jsx";
 export default function Header() {
-    return <div>Header</div>;
+    return (
+        <div className="header">
+            <div className="description">
+                <div className="title">Amphibia Memory Game</div>
+                <span>
+                    Get points by clicking on an image but don't click
+                    on any more than once!
+                </span>
+            </div>
+
+            <ScoreBoard />
+        </div>
+    );
 }
---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,8 @@
+export default function ScoreBoard() {
+    return (
+        <div className="score-board">
+            <div className="score"></div>
+            <div className="best-score"></div>
+        </div>
+    );
+}
