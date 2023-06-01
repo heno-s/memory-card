0687d2b451901c9d9528ec9a07e1a8dbcdf22f75
janko12 <henrich.sury97@gmail.com>
2023-06-01 16:40:33 +0200

    Add spacing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./style.css";
 import Header from "./components/Header.jsx";
 import CardContainer from "./components/CardContainer.jsx";
 import Footer from "./components/Footer.jsx";
+import AppStyles from "./App.module.css";
 import { useState } from "react";
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
     }
 
     return (
-        <div className="App">
+        <div className={AppStyles.app}>
             <Header score={score} bestScore={bestScore} />
             <CardContainer
                 resetGame={resetGame}
